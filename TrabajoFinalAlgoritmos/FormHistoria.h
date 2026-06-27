e4a902df714bbdf3d387ebb70a0c6f3bf8b80b7e
Dharma Romero <rdharmx@gmail.com>
2026-06-27 12:29:12 -0500

    mejorado nombre "Créditos" se agregó cinemática nivel 1, nivel 2 mas o menos

--- TrabajoFinalAlgoritmos/FormHistoria.h
+++ TrabajoFinalAlgoritmos/FormHistoria.h
@@ -8,6 +8,8 @@ namespace TodoDelMenu {
 	using namespace System::Windows::Forms;
 	using namespace System::Drawing;
 
+    public enum class PosTexto { Inferior, Superior, SoloTexto, SoloTextoCursiva, Titulo};
+
 	// Muestra una secuencia de imagenes (storyboard) y se cierra al terminar.
 	// Avanza con cualquier tecla o con click.
 	public ref class FormHistoria : public System::Windows::Forms::Form
@@ -15,13 +17,30 @@ namespace TodoDelMenu {
 	private:
 		System::ComponentModel::Container^ components;
 		List<String^>^ imagenes;
+		List<String^>^ textos;
+        List<PosTexto>^ posiciones;
 		int indice;
 
+		// Typewriter
+		String^ textoCompleto;
+		String^ textoVisible;
+		int charActual;
+		Timer^ timerTyping;
+		Label^ lblContinuar;
+
+        Image^ imagenActual;
+
 	public:
-		FormHistoria(List<String^>^ imgs)
+		FormHistoria(List<String^>^ imgs, List<String^>^ txts, List<PosTexto>^ pos)
 		{
 			imagenes = imgs;
+			textos = txts;
+			posiciones = pos;
 			indice = 0;
+			textoCompleto = "";
+			textoVisible = "";
+			charActual = 0;
+			imagenActual = nullptr;
 			InitializeComponent();
 		}
 
@@ -36,85 +55,217 @@ namespace TodoDelMenu {
 
 	private:
 #pragma region Windows Form Designer generated code
-		void InitializeComponent(void)
-		{
-			this->SuspendLayout();
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->DoubleBuffered = true;
-			this->BackColor = System::Drawing::Color::Black;
-			this->ClientSize = System::Drawing::Size(1366, 768);
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
-			this->MaximizeBox = false;
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->KeyPreview = true;
-			this->Name = L"FormHistoria";
-			this->Text = L"El Rescate de Atahualpa";
-			this->Load += gcnew System::EventHandler(this, &FormHistoria::FormHistoria_Load);
-			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &FormHistoria::FormHistoria_KeyDown);
-			this->Click += gcnew System::EventHandler(this, &FormHistoria::FormHistoria_Click);
-			this->ResumeLayout(false);
-		}
+        void InitializeComponent(void)
+        {
+            this->SuspendLayout();
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+            this->DoubleBuffered = true;
+            this->BackColor = System::Drawing::Color::Black;
+            this->ClientSize = System::Drawing::Size(1366, 768);
+            this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+            this->MaximizeBox = false;
+            this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+            this->KeyPreview = true;
+            this->Name = L"FormHistoria";
+            this->Text = L"El Rescate de Atahualpa";
+            this->Load += gcnew System::EventHandler(this, &FormHistoria::FormHistoria_Load);
+            this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &FormHistoria::FormHistoria_KeyDown);
+            this->Click += gcnew System::EventHandler(this, &FormHistoria::FormHistoria_Click);
+            this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &FormHistoria::FormHistoria_Paint);
+            this->ResumeLayout(false);
+        }
 #pragma endregion
 
 		// Dado un nombre base sin extension, busca el archivo probando extensiones comunes.
-		String^ resolverRuta(String^ base)
-		{
-			List<String^>^ exts = gcnew List<String^>();
-			exts->Add(".jpg");
-			exts->Add(".jpeg");
-			exts->Add(".png");
-			exts->Add(".bmp");
-			for each (String ^ ext in exts)
-			{
-				if (System::IO::File::Exists(base + ext))
-					return base + ext;
-			}
-			return nullptr;
-		}
+        String^ resolverRuta(String^ base)
+        {
+            if (base == nullptr) return nullptr;
+            List<String^>^ exts = gcnew List<String^>();
+            exts->Add(".jpg");
+            exts->Add(".jpeg");
+            exts->Add(".png");
+            exts->Add(".bmp");
+            for each (String ^ ext in exts)
+                if (System::IO::File::Exists(base + ext))
+                    return base + ext;
+            return nullptr;
+        }
 
-		void mostrarImagen()
+		void iniciarDiapositiva()
 		{
-			if (indice >= 0 && indice < imagenes->Count)
+			// Imagen (nullptr = pantalla negra)
+			imagenActual = nullptr;
+			this->BackgroundImage = nullptr;
+			if (indice < imagenes->Count && imagenes[indice] != nullptr)
 			{
 				String^ ruta = resolverRuta(imagenes[indice]);
 				if (ruta != nullptr)
-				{
-					this->BackgroundImage = Image::FromFile(ruta);
-					this->BackgroundImageLayout = ImageLayout::Stretch;
-				}
-				else
-				{
-					this->BackgroundImage = nullptr;
-				}
+					imagenActual = Image::FromFile(ruta);
 			}
+
+			// Texto
+			textoCompleto = (textos != nullptr && indice < textos->Count)
+				? textos[indice] : "";
+			textoVisible = "";
+			charActual = 0;
+
+			lblContinuar->Visible = false;
+
+			if (textoCompleto->Length > 0)
+				timerTyping->Start();
+			else
+				lblContinuar->Visible = true;
+
+			this->Invalidate();
 		}
 
-		void avanzar()
+		void accionAvanzar()
 		{
-			indice++;
-			if (indice >= imagenes->Count)
+			if (charActual < textoCompleto->Length)
 			{
-				this->Close();
+				timerTyping->Stop();
+				textoVisible = textoCompleto;
+				charActual = textoCompleto->Length;
+				lblContinuar->Visible = true;
+				this->Invalidate();
 			}
 			else
 			{
-				mostrarImagen();
+				indice++;
+				if (indice >= posiciones->Count)
+					this->Close();
+				else
+					iniciarDiapositiva();
 			}
 		}
 
 		System::Void FormHistoria_Load(System::Object^ sender, System::EventArgs^ e)
 		{
-			mostrarImagen();
+			lblContinuar = gcnew Label();
+			lblContinuar->Text = L"[ Presiona cualquier tecla ]";
+			lblContinuar->Font = gcnew Drawing::Font("Bahnschrift", 11, FontStyle::Italic);
+			lblContinuar->ForeColor = Color::FromArgb(200, 212, 175, 55);
+			lblContinuar->BackColor = Color::Transparent;
+			lblContinuar->AutoSize = true;
+			lblContinuar->Location = Point(950, 724);
+			lblContinuar->Visible = false;
+			this->Controls->Add(lblContinuar);
+
+			timerTyping = gcnew Timer();
+			timerTyping->Interval = 35;
+			timerTyping->Tick += gcnew EventHandler(this, &FormHistoria::timerTyping_Tick);
+
+			iniciarDiapositiva();
 		}
 
-		System::Void FormHistoria_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
+		System::Void timerTyping_Tick(System::Object^ sender, System::EventArgs^ e)
 		{
-			avanzar();
+			if (charActual < textoCompleto->Length)
+			{
+				charActual++;
+				textoVisible = textoCompleto->Substring(0, charActual);
+				this->Invalidate();
+			}
+			else
+			{
+				timerTyping->Stop();
+				lblContinuar->Visible = true;
+				this->Invalidate();
+			}
+		}
+
+		System::Void FormHistoria_Paint(System::Object^ sender,
+			System::Windows::Forms::PaintEventArgs^ e)
+		{
+			Graphics^ g = e->Graphics;
+			int W = this->ClientSize.Width;
+			int H = this->ClientSize.Height;
+
+			// Dibujar imagen si hay una
+			if (imagenActual != nullptr)
+				g->DrawImage(imagenActual, 0, 0, W, H);
+
+			if (textoVisible == nullptr || textoVisible->Length == 0)
+				return;
+
+			PosTexto pos = posiciones[indice];
+
+			// Caso 0: título grande rojo centrado
+			if (pos == PosTexto::Titulo)
+			{
+				System::Drawing::Font^ fuente = gcnew System::Drawing::Font(
+					"Bahnschrift", 48.0f, FontStyle::Bold);
+				SolidBrush^ pincel = gcnew SolidBrush(Color::FromArgb(220, 60, 0));
+
+				SizeF sz = g->MeasureString(textoVisible, fuente,
+					System::Drawing::Size(W - 160, H - 80));
+				RectangleF area(
+					(float)80,
+					(float)((H - sz.Height) / 2.0f),
+					(float)(W - 160),
+					(float)(H - 80));
+				g->DrawString(textoVisible, fuente, pincel, area);
+
+				delete pincel; delete fuente;
+				return;
+			}
+
+			// Caso 1: solo texto centrado en negro
+			if (pos == PosTexto::SoloTexto || pos == PosTexto::SoloTextoCursiva)
+			{
+				bool cursiva = (pos == PosTexto::SoloTextoCursiva);
+				System::Drawing::Font^ fuente = gcnew System::Drawing::Font(
+					"Bahnschrift",
+					cursiva ? 30.0f : 16.0f,
+					cursiva ? FontStyle::Italic : FontStyle::Regular);
+				SolidBrush^ pincel = gcnew SolidBrush(Color::White);
+
+				// Centrar verticalmente
+				SizeF sz = g->MeasureString(textoVisible, fuente,
+					System::Drawing::Size(W - 160, H - 80));
+				RectangleF area(
+					(float)80,
+					(float)((H - sz.Height) / 2.0f),
+					(float)(W - 160),
+					(float)(H - 80));
+				g->DrawString(textoVisible, fuente, pincel, area);
+
+				delete pincel; delete fuente;
+				return;
+			}
+
+			// Caso 2: caja sobre imagen (Inferior o Superior)
+			bool esInferior = (pos == PosTexto::Inferior);
+			int boxH = 130;
+			int boxY = esInferior ? (H - boxH - 10) : 10;
+
+			SolidBrush^ fondoCaja = gcnew SolidBrush(Color::FromArgb(200, 20, 12, 5));
+			Pen^ borde = gcnew Pen(Color::FromArgb(220, 212, 175, 55), 2);
+			g->FillRectangle(fondoCaja, 40, boxY, W - 80, boxH);
+			g->DrawRectangle(borde, 40, boxY, W - 80, boxH);
+			delete fondoCaja; delete borde;
+
+			System::Drawing::Font^ fuente = gcnew System::Drawing::Font(
+				"Bahnschrift", 13, FontStyle::Regular);
+			SolidBrush^ pincel = gcnew SolidBrush(Color::White);
+			RectangleF area(
+				(float)(40 + 18),
+				(float)(boxY + 14),
+				(float)(W - 80 - 36),
+				(float)(boxH - 28));
+			g->DrawString(textoVisible, fuente, pincel, area);
+			delete pincel; delete fuente;
+		}
+
+		System::Void FormHistoria_KeyDown(System::Object^ sender,
+			System::Windows::Forms::KeyEventArgs^ e)
+		{
+			accionAvanzar();
 		}
 
 		System::Void FormHistoria_Click(System::Object^ sender, System::EventArgs^ e)
 		{
-			avanzar();
+			accionAvanzar();
 		}
 	};
 }
--- TrabajoFinalAlgoritmos/MyForm.h
+++ TrabajoFinalAlgoritmos/MyForm.h
@@ -4,6 +4,7 @@
 #include "FormCreditos.h"
 #include "FormSalir.h"
 #include "FormNivel1.h"
+#include "FormNivel2.h"
 #include "FormHistoria.h"
 
 namespace TodoDelMenu {
@@ -56,7 +57,7 @@ namespace TodoDelMenu {
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// M�todo necesario para admitir el Dise�ador. No se puede modificar
+		/// Método necesario para admitir el Dise�ador. No se puede modificar
 		/// el contenido de este m�todo con el editor de c�digo.
 		/// </summary>
 		void InitializeComponent(void)
@@ -70,7 +71,7 @@ namespace TodoDelMenu {
 			// Mejora visual
 			this->DoubleBuffered = true;
 
-			// Tama�o del formulario
+			// Tamaño del formulario
 			this->ClientSize = System::Drawing::Size(1366, 768);
 
 			// No redimensionar
@@ -80,7 +81,7 @@ namespace TodoDelMenu {
 			// Centrado
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 
-			// Nombre y t�tulo
+			// Nombre y título
 			this->Name = L"MyForm";
 			this->Text = L"El Rescate de Atahualpa";
 
@@ -95,11 +96,11 @@ namespace TodoDelMenu {
 	{
 		this->SuspendLayout();
 
-		// Fondo del men�
+		// Fondo del menú
 		this->BackgroundImage = Image::FromFile("img/pantallaMenuPrincipal.jpeg");
 		this->BackgroundImageLayout = ImageLayout::Stretch; //Estira la imagen para el formulario
 
-		// T�TULO DEL JUEGO
+		// TÍTULO DEL JUEGO
 		lblTitulo = gcnew Label();
 		lblTitulo->Text = L"EL RESCATE DE ATAHUALPA";
 		lblTitulo->Font = gcnew Drawing::Font("Bahnschrift", 42, FontStyle::Bold);
@@ -111,7 +112,6 @@ namespace TodoDelMenu {
 		lblTitulo->Location = Point(350, 80);
 
 		// BOTONES
-
 		btnJugar = gcnew Button();
 		btnInstrucciones = gcnew Button();
 		btnRanking = gcnew Button();
@@ -122,10 +122,10 @@ namespace TodoDelMenu {
 		btnJugar->Text = L"JUGAR";
 		btnInstrucciones->Text = L"INSTRUCCIONES";
 		btnRanking->Text = L"RANKING";
-		btnCreditos->Text = L"CR�DITOS";
+		btnCreditos->Text = L"CR\u00c9DITOS";
 		btnSalir->Text = L"SALIR";
 
-		// Tama�o
+		// Tamaño
 		btnJugar->Size = Drawing::Size(300, 50);
 		btnInstrucciones->Size = Drawing::Size(300, 50);
 		btnRanking->Size = Drawing::Size(300, 50);
@@ -169,34 +169,214 @@ namespace TodoDelMenu {
 		
 
 		//Jugar
-
 		System::Void btnJugar_Click(System::Object^ sender, System::EventArgs^ e)
 		{
-			// Historia del Nivel 1 (storyboard)
-			List<String^>^ historia = gcnew List<String^>();
-			historia->Add("img/n1_historia1");
-			historia->Add("img/n1_historia2");
-			historia->Add("img/n1_historia3");
-			historia->Add("img/n1_historia4");
-			historia->Add("img/n1_historia5");
+			// Historia del Nivel 1 (storyboard) *********************************
+
+			List<String^>^ historia1 = gcnew List<String^>();
+			historia1->Add(nullptr);
+			//historia1->Add("img/n1_historia1");
+			historia1->Add(nullptr);
+			//historia1->Add("img/n1_historia2");
+			historia1->Add(nullptr);
+			//historia1->Add("img/n1_historia3");
+			historia1->Add("img/n1_historia3_op2");
+			//historia1->Add("img/n1_historia4");
+			historia1->Add("img/n1_historia4_op2");
+			historia1->Add("img/n1_historia5");
+
+			// cinemática nivel 1
+			List<String^>^ textos1 = gcnew List<String^>();
+			textos1->Add(
+				"Cajamarca, noviembre de 1532. El Sapa Inca Atahualpa ha sido capturado. "
+				"Los espa\u00f1oles exigen un rescate inmenso: llenar un cuarto entero de oro y plata. "
+				"El Imperio tiembla. Los chasquis, mensajeros del viento, son los \u00fanicos que pueden "
+				"llegar a los cuatro suyos a tiempo. T\u00fa eres Cusi. Y el Imperio te necesita.");
+			textos1->Add("NIVEL 1: El Camino del Chinchaysuyo");
+			textos1->Add(
+				"\u00bfPuede un mensaje cambiar el destino de un pueblo?");
+			textos1->Add(
+				"Tras la captura de Atahualpa, el Tahuantinsuyo atraviesa momentos de incertidumbre. "
+				"Cusi, un joven chasqui, recibe una misi\u00f3n crucial: recorrer el Qhapaq \u00d1an hacia el norte, "
+				"llevar el mensaje a los curacas aliados y recuperar los objetos ceremoniales que representan "
+				"la memoria y el legado de su pueblo antes de que desaparezcan para siempre.");
+			textos1->Add(
+				"Cusi ha llegado al Chinchaysuyo. El curaca local le indica la ubicaci\u00f3n de tres objetos "
+				"ceremoniales en peligro. Recupera la m\u00e1scara de oro, el textil ceremonial y la vasija ritual "
+				"mientras evitas a los soldados espa\u00f1oles que patrullan la regi\u00f3n.");
+
+			List<TodoDelMenu::PosTexto>^ posiciones1 = gcnew List<TodoDelMenu::PosTexto>();
+			posiciones1->Add(TodoDelMenu::PosTexto::SoloTexto);
+			posiciones1->Add(TodoDelMenu::PosTexto::Titulo);
+			posiciones1->Add(TodoDelMenu::PosTexto::SoloTextoCursiva);
+			posiciones1->Add(TodoDelMenu::PosTexto::Inferior);
+			posiciones1->Add(TodoDelMenu::PosTexto::Superior);
 
 			this->Hide();
 
-			FormHistoria^ intro = gcnew FormHistoria(historia);
-			intro->ShowDialog();
+			TodoDelMenu::FormHistoria^ intro1 =
+				gcnew TodoDelMenu::FormHistoria(historia1, textos1, posiciones1);
+			intro1->ShowDialog();
 
 			// Escenario jugable
 			Juego* juego = new Juego();
-			TrabajoFinalAlgoritmos::FormNivel1^ nivel = gcnew TrabajoFinalAlgoritmos::FormNivel1(juego);
-			nivel->ShowDialog();
-			delete nivel;
-			delete juego;
+			TrabajoFinalAlgoritmos::FormNivel1^ nivel1 =
+				gcnew TrabajoFinalAlgoritmos::FormNivel1(juego);
+			nivel1->ShowDialog();
+			delete nivel1;
 
+			// post nivel 1
+			if (juego->getEstado() == EstadoJuego::Derrota) {
+				List<String^>^ imgs_lost1 = gcnew List<String^>();
+				//imgs_lost1->Add("img/n1_lost");
+				imgs_lost1->Add("img/n1_fondo");
+
+				// escenario YOU LOST *************************
+				List<String^>^ txts_lost1 = gcnew List<String^>();
+				txts_lost1->Add(
+					"Cusi no logr\u00f3 recuperar los objetos ceremoniales a tiempo. "
+					"Los soldados espa\u00f1oles tomaron las ofrendas sagradas del Chinchaysuyo "
+					"y muchas fueron fundidas para convertirse en oro sin historia ni memoria.\n"
+					"El mensaje no lleg\u00f3 completo a los curacas aliados, y parte del conocimiento "
+					"del Tahuantinsuyo comenz\u00f3 a desaparecer en los caminos del norte.");
+
+				List<TodoDelMenu::PosTexto>^ pos_lost1 = gcnew List<TodoDelMenu::PosTexto>();
+				pos_lost1->Add(TodoDelMenu::PosTexto::Inferior);
+
+				TodoDelMenu::FormHistoria^ lost1 =
+					gcnew TodoDelMenu::FormHistoria(imgs_lost1, txts_lost1, pos_lost1);
+				lost1->ShowDialog();
+
+				delete juego;
+				this->Show();
+				return;
+			}
+
+			// escenario YOU WIN ********************************
+			List<String^>^ imgs_win1 = gcnew List<String^>();
+			//imgs_win1->Add("img/n1_win");
+			imgs_win1->Add("img/n1_fondo");
+
+			List<String^>^ txts_win1 = gcnew List<String^>();
+			txts_win1->Add(
+				"Los objetos ceremoniales han sido recuperados. La m\u00e1scara de oro, el textil sagrado "
+				"y la vasija ritual vuelven a estar bajo resguardo antes de caer en manos de los invasores. "
+				"El curaca del Chinchaysuyo reconoce el valor de Cusi y confirma que el mensaje podr\u00e1 "
+				"continuar su recorrido hacia el Collasuyo.\n"
+				"Pero los soldados espa\u00f1oles contin\u00faan patrullando los caminos del Qhapaq \u00d1an. "
+				"Cada objeto salvado preserva una parte de la memoria del Tahuantinsuyo, "
+				"y las alturas del sur esconden desaf\u00edos a\u00fan m\u00e1s peligrosos.");
+
+			List<TodoDelMenu::PosTexto>^ pos_win1 = gcnew List<TodoDelMenu::PosTexto>();
+			pos_win1->Add(TodoDelMenu::PosTexto::Inferior);
+
+			TodoDelMenu::FormHistoria^ win1 =
+				gcnew TodoDelMenu::FormHistoria(imgs_win1, txts_win1, pos_win1);
+			win1->ShowDialog();
+
+			//***********************************************
+			// 
+			// Historia del Nivel 2 (storyboard)
+			List<String^>^ historia2 = gcnew List<String^>();
+			historia2->Add(nullptr);
+			historia2->Add(nullptr);
+			historia2->Add("img/n2_historia3_op2");
+			//historia2->Add("img/n2_historia4");
+			historia2->Add("img/n1_fondo");
+
+			List<String^>^ textos2 = gcnew List<String^>();
+			textos2->Add("NIVEL 2: Los Pasos del Collasuyo");
+			textos2->Add(
+				"\u00bfQu\u00e9 se pierde cuando desaparece un objeto que nadie sabe que exist\u00eda?");
+			textos2->Add(
+				"Con el Chinchaysuyo informado, Cusi contin\u00faa su viaje hacia el Collasuyo. "
+				"En las alturas del sur, el fr\u00edo endurece los caminos y la desconfianza divide a los pueblos. "
+				"Los fragmentos del quipu sagrado fueron ocultados para proteger el conocimiento del Imperio, "
+				"pero separados pierden su significado.\n"
+				"Ahora Cusi deber\u00e1 reunir los fragmentos dispersos, ganarse la confianza de los chasquis "
+				"aliados y atravesar las puertas de piedra antes de que los invasores encuentren "
+				"el conocimiento oculto del Tahuantinsuyo.");
+			textos2->Add("");   // n2_historia4 se usa al TERMINAR el nivel, no al inicio
+
+			List<TodoDelMenu::PosTexto>^ posiciones2 = gcnew List<TodoDelMenu::PosTexto>();
+			posiciones2->Add(TodoDelMenu::PosTexto::Titulo);
+			posiciones2->Add(TodoDelMenu::PosTexto::SoloTextoCursiva);
+			posiciones2->Add(TodoDelMenu::PosTexto::Inferior);
+			posiciones2->Add(TodoDelMenu::PosTexto::SoloTexto);
+
+			//introduccion nivel 2
+			TodoDelMenu::FormHistoria^ intro2 =
+				gcnew TodoDelMenu::FormHistoria(historia2, textos2, posiciones2);
+			intro2->ShowDialog();
+			delete intro2;
+
+			// Nivel 2 jugable
+			Juego* juego2 = new Juego();
+			juego2->getJugador()->setVidas(juego->getJugador()->getVidas());   // conserva vidas
+			juego2->getJugador()->setPuntaje(juego->getJugador()->getPuntaje()); // conserva puntaje
+			TrabajoFinalAlgoritmos::FormNivel2^ nivel2 =
+				gcnew TrabajoFinalAlgoritmos::FormNivel2(juego2);
+			nivel2->ShowDialog();
+			delete nivel2;
+
+			// post nivel 2
+			if (juego2->getEstado() == EstadoJuego::Victoria) {
+
+				// escenario YOU LOST
+				List<String^>^ imgs_lost2 = gcnew List<String^>();
+				//imgs_cierre->Add("img/n2_historia4_op2");
+				imgs_lost2->Add("img/n1_fondo");
+			
+				List<String^>^ txts_lost2 = gcnew List<String^>();
+				txts_lost2->Add(
+					"Los fragmentos del quipu han sido reunidos. Lo que antes era solo un conjunto "
+					"de hilos dispersos vuelve a contener la memoria del Imperio. Rimaq y Wayra reconocen "
+					"a Cusi como un verdadero mensajero del Tahuantinsuyo y le abren el paso hacia el "
+					"siguiente territorio.\n"
+					"Pero mientras el conocimiento recupera su forma, las patrullas espa\u00f1olas avanzan "
+					"cada vez m\u00e1s cerca. El tiempo se agota, y el camino hacia el Antisuyo "
+					"ser\u00e1 a\u00fan m\u00e1s peligroso.");
+
+				List<TodoDelMenu::PosTexto>^ pos_lost2 = gcnew List<TodoDelMenu::PosTexto>();
+				pos_lost2->Add(TodoDelMenu::PosTexto::Inferior);
+
+				TodoDelMenu::FormHistoria^ lost2 =
+					gcnew TodoDelMenu::FormHistoria(imgs_lost2, txts_lost2, pos_lost2);
+				lost2->ShowDialog();
+
+				delete juego;
+				delete juego2;
+				this->Show();
+				return;
+			}
+
+			// escenario YOU WIN
+			List<String^>^ imgs_win2 = gcnew List<String^>();
+			imgs_win2->Add("img/n1_fondo");
+
+			List<String^>^ txts_win2 = gcnew List<String^>();
+			txts_win2->Add(
+				"Los fragmentos del quipu han sido reunidos. Lo que antes era solo un conjunto "
+				"de hilos dispersos vuelve a contener la memoria del Imperio. Rimaq y Wayra reconocen "
+				"a Cusi como un verdadero mensajero del Tahuantinsuyo y le abren el paso "
+				"hacia el siguiente territorio.\n"
+				"Pero mientras el conocimiento recupera su forma, las patrullas espa\u00f1olas avanzan "
+				"cada vez m\u00e1s cerca. El tiempo se agota, y el camino hacia el Antisuyo "
+				"ser\u00e1 a\u00fan m\u00e1s peligroso.");
+
+			List<TodoDelMenu::PosTexto>^ pos_win2 = gcnew List<TodoDelMenu::PosTexto>();
+			pos_win2->Add(TodoDelMenu::PosTexto::Inferior);
+
+			TodoDelMenu::FormHistoria^ win2 =
+				gcnew TodoDelMenu::FormHistoria(imgs_win2, txts_win2, pos_win2);
+			win2->ShowDialog();
+
+			delete juego;
+			delete juego2;
 			this->Show();
 		}
 
 		//Instrucciones
-
 		System::Void btnInstrucciones_Click(System::Object^ sender, System::EventArgs^ e)
 		{
 			FormInstrucciones^ ventanaInstrucciones = gcnew FormInstrucciones(this);
@@ -206,7 +386,6 @@ namespace TodoDelMenu {
 		}
 
 		//Ranking
-
 		System::Void btnRanking_Click(System::Object^ sender, System::EventArgs^ e)
 		{
 			FormRanking^ ventanaRanking = gcnew FormRanking(this);
@@ -216,7 +395,6 @@ namespace TodoDelMenu {
 		}
 
 		//Creditos
-
 		System::Void btnCreditos_Click(System::Object^ sender, System::EventArgs^ e)
 		{
 			FormCreditos^ ventanaCreditos = gcnew FormCreditos(this);
@@ -226,8 +404,6 @@ namespace TodoDelMenu {
 		}
 
 		//Salir
-
-
 		System::Void btnSalir_Click(System::Object^ sender, System::EventArgs^ e)
 		{
 			FormSalir^ ventanaSalir = gcnew FormSalir();
@@ -236,8 +412,5 @@ namespace TodoDelMenu {
 
 			ventanaSalir->ShowDialog();
 		}
-
-
-
 	};
 }
